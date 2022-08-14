73aa654587b28b13abe9524671329ce049895a1b
Meghanasveeramallu <86618236+Meghanasveeramallu@users.noreply.github.com>
2022-08-14 13:21:50 +0530

    Create Reverse String.java

---
+++ Reverse String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n=s.length;
+        if (n!=0 && n!=1){
+            for (int i=0;i<n/2;i++){
+                char t=s[i];
+                s[i]=s[n-i-1];
+                s[n-i-1]=t;
+            }
+        }
+        
+    }
+}
