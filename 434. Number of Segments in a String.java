a5a191b3ae402efe6f52ab7776721f7f9e3924bf
Meghanasveeramallu <86618236+Meghanasveeramallu@users.noreply.github.com>
2022-08-25 22:32:23 +0530

    Create 434. Number of Segments in a String.java

---
+++ 434. Number of Segments in a String.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countSegments(String s) {
+        s = s.trim();
+        if(s.length() == 0) return 0;
+        boolean f = false;
+        int numberOfSpaces = 1;
+        for(int i = 0; i < s.length(); i++) {
+        	if(s.charAt(i) == ' ' && f) {
+        		numberOfSpaces++;
+        		f = false;
+        	} else if(s.charAt(i) != ' ') {
+        		f = true;
+        	}
+        }
+        return numberOfSpaces;
+    }
+}
