05c2b07174b3cac1a88ae61853d466037f929324
Meghanasveeramallu <86618236+Meghanasveeramallu@users.noreply.github.com>
2022-12-01 21:55:46 +0530

    Update 1704. Determine if String Halves Are Alike.cpp

--- 1704. Determine if String Halves Are Alike.cpp
+++ 1704. Determine if String Halves Are Alike.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool halvesAreAlike(string s) {
         int answer = 0, limit = s.size()/2, n = s.size();
-        for(int i = 0; i < limit; i++)
-        if(s[i] == 'a' || s[i] == 'A' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U' || s[i] == 'e' || s[i] == 'E' ) answer++;
-        
-        for(int i = limit; i < n; i++)
-        if(s[i] == 'a' || s[i] == 'A' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U' || s[i] == 'e' || s[i] == 'E' ) answer--;
+        for(int i = 0; i < limit; i++){
+            if(s[i] == 'a' || s[i] == 'A' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U' || s[i] == 'e' || s[i] == 'E' ) answer++;
+            
+            if(s[n-i-1] == 'a' || s[n-i-1] == 'A' || s[n-i-1] == 'i' || s[n-i-1] == 'I' || s[n-i-1] == 'o' || s[n-i-1] == 'O' || s[n-i-1] == 'u' || s[n-i-1] == 'U' || s[n-i-1] == 'e' || s[n-i-1] == 'E' ) answer--;
+
+        }
 
         return !answer;
     }
